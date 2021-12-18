6f107054963b3ef5d78283fae2286ebe93140a2d
USER <abarkansalim@gmail.com>
2021-12-18 18:23:59 +0100

    Création de l’interface ClientRepository et son implémentation ClientRepositoryImpl dans le package “cigma.pfe.repositories”

---
+++ src/main/java/cigma/pfe/repositories/ClientRepositoryImpl.java
@@ -0,0 +1,10 @@
+package cigma.pfe.repositories;
+
+import cigma.pfe.models.Client;
+
+public class ClientRepositoryImpl implements IClientRepository{
+    @Override
+    public Client save(Client c) {
+        return null;
+    }
+}
