5b27da417a20425fdd98ba628d9f3ba60c5a254b
USER <abarkansalim@gmail.com>
2021-12-18 23:26:26 +0100

    scope="prototype" + context.getBean("controller");

--- src/main/java/cigma/pfe/MonApplication.java
+++ src/main/java/cigma/pfe/MonApplication.java
@@ -7,5 +7,6 @@ public class MonApplication {
     public static void main(String[] args) {
         ApplicationContext context = new
                 ClassPathXmlApplicationContext("spring.xml");
+        context.getBean("controller");
     }
 }
