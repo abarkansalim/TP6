e57b060cd5fad68df051eeb48b182b91a7a338f8
USER <abarkansalim@gmail.com>
2021-12-18 19:42:37 +0100

    Création de l’interface ClientService et son implémentation ClientServiceImpl dans le package “cigma.pfe.services”

---
+++ src/main/java/cigma/pfe/services/ClientServiceImpl.java
@@ -0,0 +1,15 @@
+package cigma.pfe.services;
+
+import cigma.pfe.models.Client;
+import cigma.pfe.repositories.ClientRepositoryImpl;
+import cigma.pfe.repositories.IClientRepository;
+
+public class ClientServiceImpl implements IClientService{
+    IClientRepository clientRepository = new ClientRepositoryImpl();
+
+    @Override
+    public Client save(Client c) {
+        System.out.println("Service Layer : ClientServiceImpl Level... ");
+        return clientRepository.save(c);
+    }
+}
